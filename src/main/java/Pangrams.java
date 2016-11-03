6479d90d9c3eae773c88e1b0b18378bcc32144d9
Tianming Zhuang <tianming.zhuang@gmail.com>
2016-11-03 15:52:59 -0400

    Implemented using streams
    Much more compact implementation using Streams.

--- src/main/java/Pangrams.java
+++ src/main/java/Pangrams.java
@@ -4,18 +4,10 @@ class Pangrams {
   private static final int ALPHABET_SIZE = 26;
 
   public static boolean isPangram(String sentence) {
-    Map<Character, Character> foundLetters = new HashMap<Character, Character>();
-    sentence = normalizeSentence(sentence);
-    char[] letters = sentence.toCharArray();
-
-    for(char c: letters) {
-      foundLetters.put(c, c);
-    }
-
-    return foundLetters.size() == ALPHABET_SIZE;
-  }
-
-  private static String normalizeSentence(String sentence) {
-    return sentence.toLowerCase().replaceAll("[^a-z]", "");
+    return sentence.toLowerCase()
+      .replaceAll("[^a-z]", "")
+      .chars()
+      .distinct()
+      .count() == ALPHABET_SIZE;
   }
 }
